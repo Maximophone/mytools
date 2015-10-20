5b737b009e9c86aa3f9a35d96ac1501d26ad2a86
m.fournes <fournes.maxime@gmail.com>
2015-10-20 20:37:42 +0100

    Added functionality to play go

--- mytools/communicator.py
+++ mytools/communicator.py
@@ -1,11 +1,41 @@
 from com import ChatServer, ChatClient, AESCipher
-
 from pygo import Go, Human
+
+class PlayerChat(Human):
+	def __init__(self,name,chat):
+		self.chat = chat
+		super(PlayerChat,self).__init__(name)
+
+	def decide(self,board,color):
+		coords = raw_input()
+		self.chat.sendall(coords)
+		if coords == 'pass': return None
+		if coords == 'exit': raise ExitException()
+		i,j = self._parse_coords(coords)
+		i=int(i)
+		j=int(j)
+		return (i,j)
+
+class OpponentChat(Human):
+	def __init__(self,name,chat):
+		self.chat = chat
+		self.iterator = chat.iter_received()
+		super(OpponentChat,self).__init__(name)
+
+	def decide(self,board,color):
+		coords = self.iterator.next().split(': ')[-1]
+		if coords == 'pass': return None
+		if coords == 'exit': raise ExitException()
+		i,j = self._parse_coords(coords)
+		i=int(i)
+		j=int(j)
+		return (i,j)
 
 class Communicator(object):
 	def __init__(self):
 		self.commands_lookup = {
 			'!chat':'start_chat',
+			'!go':'start_go',
 			'!help':'show_help'
 		}
 
@@ -13,6 +43,22 @@ class Communicator(object):
 		print 'Communicator help'
 		print 'List of available commands:'
 		print self.commands_lookup
+
+	def start_go(self):
+		ip_addr = raw_input('What IP (or type listen)?: ')
+		name = raw_input('Name:')
+		aes_cipher = AESCipher(raw_input('key: '))
+		chat = None
+		if ip_addr.lower() == 'listen':
+			chat = ChatServer(name,aes_cipher)
+		else:
+			chat = ChatClient(name,aes_cipher)
+			chat.host = ip_addr
+		player = PlayerChat(name,chat)
+		opponent = OpponentChat('Opponent',chat)
+		chat.start()
+		if ip_addr.lower() == 'listen': go = Go(9,player1=player,player2=opponent)
+		else: go = Go(9,player1=opponent,player2=player)
 
 	def start_chat(self):
 		ip_addr = raw_input('What IP (or type listen)?: ')
